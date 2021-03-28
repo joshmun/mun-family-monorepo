af01a24fbf03bd470a0403305104a6d7cfdbbc66
joshmun <jmun@medallia.com>
2021-03-27 20:00:21 -0500

    add backgruond image

--- family-app/App.tsx
+++ family-app/App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const image = require("./assets/engagement.jpeg");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the Mun Family app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} style={styles.image} blurRadius={7}>
+        {/* <View style={styles.overlay}> */}
+        <Text style={styles.text}>Welcome to the Mun Family app!</Text>
+        {/* </View> */}
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,7 +20,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: "column",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    tintColor: "#000000",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: "20",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.5)",
+    height: "100vh",
     alignItems: "center",
     justifyContent: "center",
   },
